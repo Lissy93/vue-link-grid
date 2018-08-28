be8ac13c0fd39583267cac5683e9e94f1d979aba
Alicia Sykes <sykes.alicia@gmail.com>
2018-08-28 11:07:53 +0100

    Makes a blank componento

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TileSection tileSectionTitle="section title" tileSectionSubTitle="demo subtitle"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import TileSection from './components/TileSection.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    TileSection,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/TileSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <header>
+        <h2>{{ tileSectionTitle }}</h2>
+        <span> {{ tileSectionSubTitle }} </span>
+    </header>
+    <div>
+        
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TileSection extends Vue {
+  @Prop() private tileSectionTitle!: string;
+  @Prop() private tileSectionSubTitle?: string;
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
